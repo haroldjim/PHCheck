9f207ccf88535a38f2e59f91b04bd5d387b48d00
haroldjim <haroldjimcamino@gmail.com>
2014-06-20 15:13:16 +0800

    DAL
    DAL for test School

---
+++ SchoolContext.vb
@@ -0,0 +1,21 @@
+﻿Imports Contoso_University.Models
+Imports System.Data.Entity
+Imports System.Data.Entity.ModelConfiguration.Conventions
+
+Namespace DAL
+    Public Class SchoolContext
+        Inherits DbContext
+
+        Public Sub New()
+            MyBase.New("dbContext")
+        End Sub
+
+        Public Property Students As DbSet(Of Student)
+        Public Property Enrollments As DbSet(Of Enrollment)
+        Public Property Courses As DbSet(Of Course)
+
+        Protected Overrides Sub OnModelCreating(ByVal modelBuilder As DbModelBuilder)
+            modelBuilder.Conventions.Remove(Of PluralizingTableNameConvention)()
+        End Sub
+    End Class
+End Namespace
